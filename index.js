48c06d39b0514487d479d8a1b6686350195f8eb7
progRizvi <prog.rizvi@gmail.com>
2021-11-13 18:30:28 +0600

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require("dotenv").config()
 const cors = require("cors")
 
@@ -17,6 +18,117 @@ async function run() {
         client.connect()
         const database = client.db("squadrone")
         const productsCollection = database.collection("products")
+        const usersCollection = database.collection("users")
+        const ordersCollection = database.collection("orders")
+        const reviewsCollection = database.collection("reviews")
+        const blogsCollection = database.collection("blogs")
+        const clientsCollection = database.collection("clients")
+
+        // Get All Blogs
+        app.get("/blogs", async(req, res) => {
+                const result = await blogsCollection.find({}).toArray()
+                res.send(result)
+            })
+            // Get All Client Logo
+        app.get("/clients", async(req, res) => {
+                const result = await clientsCollection.find({}).toArray()
+                res.send(result)
+            })
+            // Get All Products
+        app.get("/products", async(req, res) => {
+                const result = await productsCollection.find({}).toArray()
+                res.send(result)
+            })
+            // Get Single Product
+        app.get("/products/:productId", async(req, res) => {
+                const productId = req.params.productId
+                const filter = { _id: ObjectId(productId) }
+                const result = await productsCollection.findOne(filter)
+                res.send(result)
+            })
+            // Add Product
+        app.post("/products", async(req, res) => {
+            const product = req.body
+            const result = await productsCollection.insertOne(product)
+            console.log(result.insertedId)
+            res.json(result)
+        })
+
+        // Delete a Product
+        app.delete("/products/:productId", async(req, res) => {
+                const product = req.params.productId
+                const query = { _id: ObjectId(product) }
+                const result = await productsCollection.deleteOne(query)
+                console.log(result)
+                res.json(result)
+            })
+            // Insert User
+        app.post("/users", async(req, res) => {
+                const user = req.body
+                const result = await usersCollection.insertOne(user)
+                res.json(result)
+            })
+            // Make Admin
+        app.get("/user", async(req, res) => {
+            const user = req.query.email
+            const filter = { userEmail: user }
+            const result = await usersCollection.findOne(filter)
+            let isAdmin = false;
+            if (result.role === "Admin") {
+                isAdmin = true;
+            }
+            res.send({ result, isAdmin })
+        })
+        app.put("/admin", async(req, res) => {
+            const email = req.body.userEmail
+            console.log(email)
+            const filter = { userEmail: email }
+            const updateDoc = { $set: { role: "Admin" } }
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.json(result)
+            console.log(result)
+        })
+        app.delete("/orders/:orderId", async(req, res) => {
+            const orderId = req.params.orderId
+            const filter = { _id: ObjectId(orderId) }
+            const result = await ordersCollection.deleteOne(filter)
+            res.json(result)
+        })
+        app.get("/allOrders", async(req, res) => {
+            const result = await ordersCollection.find({}).toArray()
+            res.send(result)
+        })
+        app.put("/orders", async(req, res) => {
+            const orderId = req.query.id
+            const update = req.body.status
+            const filter = { _id: ObjectId(orderId) }
+            const updateDoc = { $set: { status: update } }
+            const result = await ordersCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+        app.get("/orders", async(req, res) => {
+            const find = req.query.email
+            const filter = { email: find }
+            const result = await ordersCollection.find(filter).toArray()
+            res.send(result)
+        })
+        app.post("/orders/", async(req, res) => {
+            const order = req.body
+            const result = await ordersCollection.insertOne(order)
+            res.json(result)
+        })
+
+        // Post a Review
+        app.post("/reviews", async(req, res) => {
+                const review = req.body
+                const result = await reviewsCollection.insertOne(review)
+                res.json(result)
+            })
+            // Get All Reviews
+        app.get("/reviews", async(req, res) => {
+            const result = await reviewsCollection.find({}).toArray()
+            res.send(result)
+        })
     } finally {
         // await client.close()
     }
